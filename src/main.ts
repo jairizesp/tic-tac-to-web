9a8aa9a21e0df87939364ac6587b5598aa0ac266
jairizesp <esposojairizedward@gmail.com>
2024-08-04 20:34:44 +0800

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://tic-tac-toe-client-1wsgtiv6y-jairizs-projects.vercel.app/', // Update this with your production domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
